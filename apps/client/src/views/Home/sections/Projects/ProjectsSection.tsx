88999123c5203f76d64d934d1c74a38be37543c0
Abilov599 <abilovv599@gmail.com>
2024-04-19 00:07:17 +0400

    Refactor ProjectsSection component in ProjectsSection.tsx

--- apps/client/src/views/Home/sections/Projects/ProjectsSection.tsx
+++ apps/client/src/views/Home/sections/Projects/ProjectsSection.tsx
@@ -3,17 +3,17 @@
 import { projectsData } from "@/data";
 import { SectionHeading, Project } from "@/components";
 import { FC, ComponentPropsWithoutRef, Fragment } from "react";
-import { useSectionInView } from "@/hooks";
+// import { useSectionInView } from "@/hooks";
 
 interface ProjectsSectionProps extends ComponentPropsWithoutRef<"section"> {}
 
 const ProjectsSection: FC<ProjectsSectionProps> = ({ ...props }) => {
-  const { ref } = useSectionInView("Projects", 0.5);
+  // const { ref } = useSectionInView("Projects", 0.5);
   return (
     <section
       id="projects"
       className="mb-16 scroll-mt-28 sm:scroll-mt-24"
-      ref={ref}
+      // ref={ref}
       {...props}
     >
       <SectionHeading>My Projects</SectionHeading>
